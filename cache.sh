1682e2eb7c0c7e0e56721ce8d0ee46426e26d768
Demid-ns <46938257+Demid-ns@users.noreply.github.com>
2023-04-16 21:22:26 +0300

    Rename

--- cache.sh
+++ cache.sh
@@ -12,6 +12,6 @@ if [ -z "$1" ]; then
 fi
 
 # Save the CPU cache sizes to the specified file
-echo "L1 cache size: $L1_cache_size" >"$1"
+echo "L1 cache size: $L1_cache_size" >>"$1"
 echo "L2 cache size: $L2_cache_size" >>"$1"
 echo "L3 cache size: $L3_cache_size" >>"$1"
